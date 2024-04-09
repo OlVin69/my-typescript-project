490553ce76d7d1cf94bced7f059eb49d6c8a804d
OlVin69 <aviniichuk@gmail.com>
2024-04-09 09:52:14 +0300

    add tsconfig.json

--- src/components/App/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,6 +0,0 @@
-import css from './ErrorMessage.module.css'
-
-export default function ErrorMessage() {
-    return <p className={css.text}>Whoops, something went wrong! Please try reloading this page!</p>
-    
-}
